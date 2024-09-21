0d2e1edf3e62d77e1a49a599d96e6be98c8f737a
torresayalaedwinerubiel <142061844+torresayalaedwinerubiel@users.noreply.github.com>
2024-09-21 14:37:37 -0600

    tipo de datos v1
    Demostración de los tipos de datos.

--- tiposdedatos/src/tiposdedatos/main/Tiposdatos.java
+++ tiposdedatos/src/tiposdedatos/main/Tiposdatos.java
@@ -4,11 +4,14 @@ public class Tiposdatos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
-
-		System.out.println("Hola mundo");
-System.out.println("Hola mundo");
-
+		byte ejbyte = -128;
+		System.out.println("Valor mínimo " + Byte.MIN_VALUE+" valor máximo " + Byte.MAX_VALUE);
+		
+		short ejshort = 32767;
+		System.out.println("Valor mínimo " + Short.MIN_VALUE+" valor máximo " + Short.MAX_VALUE);
+		
+		int ejint = 2147483647;
+		System.out.println("Valor mínimo " + Integer.MIN_VALUE+" valor máximo " + Integer.MAX_VALUE);
 
 	}
 
